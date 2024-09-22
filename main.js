ae87f3e1a674facb419b3c1b2707444321736274
ahmed saleh <104595421+ahmedbasuony7@users.noreply.github.com>
2024-09-23 00:54:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// Define three variables:
+
+// name for your name.
+// hobby for your favorite hobby.
+// quote for a favorite quote.
+// In one statement, use a template string to construct and print a message in the 
+//following format (with an escape sequence for a new line and a tab):
+
+// Ensure you use an escape sequence to add a newline between lines, and a tab escape before the hobby
+
+
+let  namee = "Ahmed saleh" ,
+hoppyies = ["reading , codding , football"]  ,
+quote  = "Life is like riding a bicycle. To keep your balance, you must keep moving. ";
+
+
+let data = ` Name: ${namee}\n\tHobby: ${hoppyies}\nQuote: "${quote}"  `
+
+console.log(data);
+
